794cc68ad5caccb7edac3988af7515f3689f46ea
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-02-17 13:48:31 +0100

    set up keystone, updtate caniuse-lite

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,33 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // length of time user stays signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: add data seeding here
+  },
+
+  lists: createSchema({
+    // Schema items go here
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
